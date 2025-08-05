a0e695539fcf65cb74e907230245e91ebc7c7b1c
varun-developer-public <varundev.bytes@gmail.com>
2025-08-05 17:08:22 +0530

    `Update index.css to remove tailwindcss import and main.jsx to render Formss component instead of Home component.`

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './components/home/home'
+import Formss from './components/forms/forms'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <Home/>
+    <Formss/>
   </>
 )
